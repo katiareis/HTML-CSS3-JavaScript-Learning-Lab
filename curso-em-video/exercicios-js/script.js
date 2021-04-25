b2dce7105fff0ebdca4ffdcbd056bbe1a7df4116
Katia Reis <62106987+katiareis@users.noreply.github.com>
2021-04-25 05:10:53 -0300

    Day Time webpage
    Learning about html, CSS and JS.
    Add style and JS to static page

--- curso-em-video/exercicios-js/script.js
+++ curso-em-video/exercicios-js/script.js
@@ -1,14 +1,30 @@
-// Curso de JS - Digital House
+// Curso de JS - Curso em Video
 /*
-Variáveis em JS
+Primeiros comandos
 */ 
 
-if(true) {
-    var a = "Eu"
-    var b = "tenho"
-    var c = "anos"
-    var idade = 20
-    
+function carregar() {
+    var msg = window.document.getElementById('mensagem')
+    var img = window.document.getElementById('picture')
+    var greeting = window.document.getElementById('greeting')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    mensagem.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
+    if (hora >= 0 && hora <12) {
+        // Bom dia
+        img.src = "image/morning.jpg"
+        document.body.style.background = "#f5e964"
+        greeting.innerHTML = "Bom dia!" 
+    } else if (hora >= 12 && hora < 18) {
+        // Boa tarde
+        img.src = "image/afternoon.jpg"
+        document.body.style.background = "#e8ad5a"
+        greeting.innerHTML = "Boa tarde!"
+    } else {
+        // Boa noite
+        img.src = "image/night.jpg"
+        document.body.style.background = "#062775"
+        greeting.innerHTML = "Boa noite!"
     }
-    console.log(a + b + idade + c)
-    
+}
