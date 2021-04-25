74d6eba3b2ee1dbe6057ed19ad9069d975722093
Katia Reis <62106987+katiareis@users.noreply.github.com>
2021-04-25 02:52:12 -0300

    Add current time project
    Creat HTML, JavaScript and CSS basic template.

---
+++ curso-em-video/exercicios-js/script.js
@@ -0,0 +1,14 @@
+// Curso de JS - Digital House
+/*
+Variáveis em JS
+*/ 
+
+if(true) {
+    var a = "Eu"
+    var b = "tenho"
+    var c = "anos"
+    var idade = 20
+    
+    }
+    console.log(a + b + idade + c)
+    
