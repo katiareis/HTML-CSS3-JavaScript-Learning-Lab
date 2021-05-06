f3b48e9bf195a0fe0226bd31417bb32777b788ce
Katia Reis <62106987+katiareis@users.noreply.github.com>
2021-05-06 08:51:44 -0300

    Update script.js
    Learning more about JS

--- curso-em-video/exercicios-js/script.js
+++ curso-em-video/exercicios-js/script.js
@@ -13,18 +13,21 @@ function carregar() {
     mensagem.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
     if (hora >= 0 && hora <12) {
         // Bom dia
+        greeting.innerHTML = "Bom dia!" 
         img.src = "image/morning.jpg"
         document.body.style.background = "#f5e964"
-        greeting.innerHTML = "Bom dia!" 
+
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde
+        greeting.innerHTML = "Boa tarde!"
         img.src = "image/afternoon.jpg"
         document.body.style.background = "#e8ad5a"
-        greeting.innerHTML = "Boa tarde!"
+        
     } else {
         // Boa noite
+        greeting.innerHTML = "Boa noite!"
         img.src = "image/night.jpg"
         document.body.style.background = "#062775"
-        greeting.innerHTML = "Boa noite!"
+        
     }
 }
